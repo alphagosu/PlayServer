ea1a8e540be84ec2c8d5a7c87ab1b23e60472fb4
Mark <mark.ghareeb@gmail.com>
2015-11-17 00:05:45 -0500

    Implemented basic routers and routes, tutorial

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,6 +8,9 @@ import (
 // handler is a simple handler to requests.
 type handler func(http.ResponseWriter, *http.Request) error
 
+//this is a list of strings
+type listOfStrings []string
+
 // RootHandler is the homepage.
 func RootHandler(w http.ResponseWriter, r *http.Request) error {
 	fmt.Fprintf(w, "<h1>Server is running.</h1>")
@@ -15,15 +18,24 @@ func RootHandler(w http.ResponseWriter, r *http.Request) error {
 }
 
 // ErrorHandler is a wrapper for the handlers that returns and logs errors.
-func ErrorHandler(f handler, method string) http.HandlerFunc {
+func ErrorHandler(f handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != method {
-			err := fmt.Sprintf("This path only supports %s requests", method)
-			http.Error(w, err, http.StatusNotFound)
-		} else {
-			if err := f(w, r); err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-			}
+		// if !methods.contains(r.Method) {
+		// 	err := fmt.Sprintf("This path only supports the following requests: %s", strings.Join(methods, ", "))
+		// 	http.Error(w, err, http.StatusNotFound)
+		// } else {
+		if err := f(w, r); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
+}
+
+// checks to see if a value is contained in an array.
+func (vals listOfStrings) contains(s string) bool {
+	for _, val := range vals {
+		if s == val {
+			return true
+		}
+	}
+	return false
 }
--- router/router.go
+++ router/router.go
@@ -9,6 +9,43 @@ import (
 // TODO: I need to write my own router using a http.mux
 // to allow for specifying the method a path can support.
 func main() {
-	http.HandleFunc("/", handlers.ErrorHandler(handlers.RootHandler, "GET"))
-	http.ListenAndServe(":8080", nil)
+	m := http.NewServeMux()
+	r := router{
+		mux:    m,
+		routes: []route{},
+	}
+	r.routes = append(r.routes, route{
+		path: "/",
+		h:    handlers.ErrorHandler(handlers.RootHandler),
+	})
+
+	r.registerRoutes()
+
+	//validRootMethods := []string{"GET", "POST"}
+	//http.HandleFunc("/", handlers.ErrorHandler(handlers.RootHandler, validRootMethods))
+
+	http.ListenAndServe(":8080", r.mux)
+}
+
+//paths, routes, handler fucntions, router, mux
+
+// router
+// 	a route is composed of path, handler, methods(get, post.. etc)
+// 	mux
+//
+
+type router struct {
+	mux    *http.ServeMux
+	routes []route
+}
+
+type route struct {
+	path string
+	h    func(http.ResponseWriter, *http.Request)
+}
+
+func (rtr router) registerRoutes() {
+	for _, r := range rtr.routes {
+		rtr.mux.HandleFunc(r.path, r.h)
+	}
 }
