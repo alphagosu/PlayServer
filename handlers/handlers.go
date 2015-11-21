8fd9d834f4d4d718b71a4d5cba76d865d858a8df
Play <play@Brandons-MacBook-Pro.local>
2015-11-21 12:48:06 -0500

    Using server struct for more control. Next we will have to implement our own listen to have logging for all requests. This will allow for the next logical step, custom throttling for prevention of brute forcing, if we ever do logins, and DDoS attacks.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -12,8 +12,7 @@ type Handler func(http.ResponseWriter, *http.Request) error
 
 // RootHandler is the homepage.
 func RootHandler(w http.ResponseWriter, r *http.Request) error {
-	ServeContent(w, r, "<h1>Server is running.</h1>", http.StatusOK)
-	return nil
+	return ServeContent(w, r, "<h1>Server is running.</h1>", http.StatusOK)
 }
 
 // ErrorHandler is a wrapper for the handlers that returns and logs errors.
@@ -26,12 +25,21 @@ func ErrorHandler(f Handler) http.HandlerFunc {
 }
 
 // ServeContent is a wrapper for responding to clients. It can log the
-func ServeContent(w http.ResponseWriter, r *http.Request, body string, statusCode int) {
+func ServeContent(w http.ResponseWriter, r *http.Request, body string, statusCode int) error {
 	logHeader := fmt.Sprintf(time.Now().Format("Jan 2 15:04:05 EST"))
 	path := fmt.Sprintf(", request on path '%s' ", r.URL.Path)
 	status := fmt.Sprintf("returned status code: %d", statusCode)
 
+	if statusCode > 299 {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		errString := fmt.Sprintf(" with error: %s", body)
+		status = status + errString
+	}
+
 	log.Println(logHeader + path + status)
 	w.WriteHeader(statusCode)
 	fmt.Fprintln(w, body)
+
+	return nil
 }
--- router/router.go
+++ router/router.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	h "github.com/alphagosu/PlayServer/handlers"
 	"github.com/gorilla/mux"
@@ -15,9 +19,20 @@ const (
 func main() {
 	r := mux.NewRouter()
 	get(r, "/", h.RootHandler)
-	http.ListenAndServe(":8080", r)
+
+	listenAndServe(":8080", r, newLogger())
 }
 
 func get(r *mux.Router, path string, f h.Handler) {
 	r.HandleFunc("/", h.ErrorHandler(h.RootHandler)).Methods(GET)
+}
+
+func newLogger() *log.Logger {
+	logHeader := fmt.Sprintf(time.Now().Format("Jan 2 15:04:05 EST"))
+	return log.New(os.Stdout, logHeader, log.LstdFlags)
+}
+
+func listenAndServe(addr string, handler http.Handler, logger *log.Logger) error {
+	server := &http.Server{Addr: addr, Handler: handler, ErrorLog: logger}
+	return server.ListenAndServe()
 }
